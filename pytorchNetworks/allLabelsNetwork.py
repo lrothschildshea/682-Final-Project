61a94af7555ad81da92442d1b88ea082086fe8ca
lrothschildshea <lrothschildshea@gmail.com>
2018-10-24 16:15:53 -0400

    adding network for all labels

---
+++ pytorchNetworks/allLabelsNetwork.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+import numpy as np
+
+def allLabelsNetwork(learning_rate, channels):
+    model = nn.Sequential()
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    return model, optimizer
