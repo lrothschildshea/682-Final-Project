e816558bf0126184cf0e27a4c9eabaa264185b9e
Tom Aurigemma <taaurigemma@gmail.com>
2018-10-24 16:06:43 -0400

    Able to load instant of CiFAR dataset

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
 #file to import data, relabel data, etc.
+
+from __future__ import print_function
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from data_utils import get_CIFAR10_data
+
+data = get_CIFAR10_data()
+for k,v in list(data.items()):
+  print(('%s:' % k, v.shape))
