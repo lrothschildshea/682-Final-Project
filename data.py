9d05e106b34decea62e9af38c56de9d42a05be0b
Thomas Aurigemma <taaurigemma@gmail.com>
2018-10-29 15:58:07 -0400

    Relabeling data complete!

--- data.py
+++ data.py
@@ -2,19 +2,20 @@
 
 from __future__ import print_function
 import time
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np #pylint: disable= E0401
+import matplotlib.pyplot as plt #pylint: disable= E0401
 from data_utils import get_CIFAR10_data
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torch.utils.data import sampler
+import torch #pylint: disable= E0401
+import torch.nn as nn #pylint: disable= E0401
+import torch.optim as optim #pylint: disable= E0401
+from torch.utils.data import DataLoader #pylint: disable= E0401
+from torch.utils.data import sampler #pylint: disable= E0401
 
-import torchvision.datasets as dset
-import torchvision.transforms as T
+import torchvision.datasets as dset #pylint: disable= E0401
+import torchvision.transforms as T #pylint: disable= E0401
 
+hold = []
 #this method is currently not used - possibly remove in the future if not needed
 def getData():
     data = get_CIFAR10_data()
@@ -56,6 +57,7 @@ def getDataPyTorch():
                              transform=transform)
     loader_train = DataLoader(cifar10_train, batch_size=64, 
                           sampler=sampler.SubsetRandomSampler(range(NUM_TRAIN)))
+    
     cifar10_val = dset.CIFAR10('./dataset', train=True, download=True,
                            transform=transform)
     loader_val = DataLoader(cifar10_val, batch_size=64, 
@@ -69,11 +71,10 @@ def getDataPyTorch():
 
 def relabelDataPyTorch(data, score):
     loader_train, loader_val, loader_test = data
-    temp_loader_train = loader_train
-    temp_loader_val = loader_val
-    temp_loader_test = loader_test
-
-    for batch in temp_loader_train:
+    list_loader_train = []
+    list_loader_val = []
+    list_loader_test = []
+    for batch in loader_train:
         batch_labels = batch[1]
         for i in range(0,list(batch_labels.size())[0]):
             if(batch_labels[i] == torch.LongTensor([score])):
@@ -81,8 +82,26 @@ def relabelDataPyTorch(data, score):
             else:
                 batch_labels[i] = torch.LongTensor([0])
         batch[1] = batch_labels
-
-    for batch in temp_loader_train:
-        print(batch)
-
-relabelDataPyTorch(getDataPyTorch(), 2)
+        list_loader_train.append(batch)
+    
+    for batch in loader_val:
+        batch_labels = batch[1]
+        for j in range(0,list(batch_labels.size())[0]):
+            if(batch_labels[j] == torch.LongTensor([score])):
+                batch_labels[j] = torch.LongTensor([1])
+            else:
+                batch_labels[j] = torch.LongTensor([0])
+        batch[1] = batch_labels
+        list_loader_val.append(batch)
+    
+    for batch in loader_test:
+        batch_labels = batch[1]
+        for k in range(0,list(batch_labels.size())[0]):
+            if(batch_labels[k] == torch.LongTensor([score])):
+                batch_labels[k] = torch.LongTensor([1])
+            else:
+                batch_labels[k] = torch.LongTensor([0])
+        batch[1] = batch_labels
+        list_loader_test.append(batch)
+    
+    return (loader_train, list_loader_train, list_loader_val, list_loader_test)
