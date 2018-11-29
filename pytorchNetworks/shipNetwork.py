78f1f1a64e7b8ed471998be83c9c097a49f6255a
Liam Rothschild-Shea <lrothschildshea@gmail.com>
2018-11-29 16:23:23 -0500

    decent automobile and ship performance

--- pytorchNetworks/shipNetwork.py
+++ pytorchNetworks/shipNetwork.py
@@ -2,13 +2,37 @@ import torch.nn as nn
 import torch.optim as optim
 from .utils import *
 
-def shipNetwork(learning_rate, channels):
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        nn.init.xavier_uniform_(m.weight)
+
+def shipNetwork():
     model = nn.Sequential(
-        nn.Conv2d(channels[0], channels[1], (5, 5), padding=2),
+        nn.Conv2d(3, 32, (5, 5), padding=2),
         nn.ReLU(),
+        nn.Conv2d(32, 32, (3, 3), padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d((2,2), stride=2),
+        nn.Conv2d(32, 16, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.Conv2d(16, 24, (5, 5), padding=2),
+        nn.ReLU(),
+        nn.MaxPool2d((2,2), stride=2),
+        nn.Conv2d(24, 64, (3, 3), padding=1),
+        nn.ReLU(),
+        nn.Conv2d(64, 32, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.MaxPool2d((2,2), stride=2),
+        nn.Conv2d(32, 64, (9, 9), padding=4),
+        nn.ReLU(),
+        nn.Conv2d(64, 128, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.MaxPool2d((4,4), stride=2),
         Flatten(),
-        nn.Linear(channels[2]*32*32, 2),
+        nn.Linear(128, 2),
     )
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    model.apply(init_weights)
+    optimizer = optim.RMSprop(model.parameters(), lr=.0005)
 
     return model, optimizer
