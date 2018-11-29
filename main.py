28344b8a83b61ecb6e8417f3338f9b96e4c52632
Liam Rothschild-Shea <lrothschildshea@gmail.com>
2018-11-29 15:09:46 -0500

    more progress

--- main.py
+++ main.py
@@ -40,15 +40,15 @@ out = [None]*10
 all_scores = [None]*10
 
 models[0], optimizers[0] = airplaneNetwork(.13, [3, 32, 32])
-models[1], optimizers[1] = automobileNetwork(.13, [3, 32, 32])
-models[2], optimizers[2] = birdNetwork(.13, [3, 32, 32])
-models[3], optimizers[3] = catNetwork(.13, [3, 32, 32])
-models[4], optimizers[4] = deerNetwork(.13, [3, 32, 32])
-models[5], optimizers[5] = dogNetwork(.13, [3, 32, 32])
-models[6], optimizers[6] = frogNetwork(.13, [3, 32, 32])
-models[7], optimizers[7] = horseNetwork(.13, [3, 32, 32])
-models[8], optimizers[8] = shipNetwork(.13, [3, 32, 32])
-models[9], optimizers[9] = truckNetwork(.13, [3, 32, 32])
+models[1], optimizers[1] = airplaneNetwork(.13, [3, 32, 32])
+models[2], optimizers[2] = airplaneNetwork(.13, [3, 32, 32])
+models[3], optimizers[3] = airplaneNetwork(.13, [3, 32, 32])
+models[4], optimizers[4] = airplaneNetwork(.13, [3, 32, 32])
+models[5], optimizers[5] = airplaneNetwork(.13, [3, 32, 32])
+models[6], optimizers[6] = airplaneNetwork(.13, [3, 32, 32])
+models[7], optimizers[7] = airplaneNetwork(.13, [3, 32, 32])
+models[8], optimizers[8] = airplaneNetwork(.13, [3, 32, 32])
+models[9], optimizers[9] = airplaneNetwork(.13, [3, 32, 32])
 m, o = allLabelsNetwork(.01, [3, 32, 32])
 
 for i in range(NUM_LABELS):
--- pytorchNetworks/airplaneNetwork.py
+++ pytorchNetworks/airplaneNetwork.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.optim as optim
 from .utils import *
 
+
 def airplaneNetwork(learning_rate, channels):
     model = nn.Sequential(
         nn.Conv2d(3, 32, (5, 5), padding=2),
@@ -19,9 +20,14 @@ def airplaneNetwork(learning_rate, channels):
         nn.Conv2d(64, 32, (7, 7), padding=3),
         nn.ReLU(),
         nn.MaxPool2d((2,2), stride=2),
+        nn.Conv2d(32, 64, (9, 9), padding=4),
+        nn.ReLU(),
+        nn.Conv2d(64, 128, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.MaxPool2d((4,4), stride=2),
         Flatten(),
-        nn.Linear(512, 2),
+        nn.Linear(128, 2),
     )
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = optim.RMSprop(model.parameters(), lr=.01)
 
     return model, optimizer
