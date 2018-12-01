8de2a9a201784ef1f40bb34b283131b2fc64edd7
Liam Rothschild-Shea <lrothschildshea@gmail.com>
2018-11-30 22:56:44 -0500

    okay truck network

--- main.py
+++ main.py
@@ -39,24 +39,24 @@ lltst = [None]*10
 out = [None]*10
 all_scores = [None]*10
 
-models[0], optimizers[0] = airplaneNetwork(.13, [3, 32, 32])        #airplane
+models[0], optimizers[0] = airplaneNetwork()        #airplane
 models[1], optimizers[1] = automobileNetwork()      #automobile
-models[2], optimizers[2] = airplaneNetwork(.13, [3, 32, 32])        #bird
-models[3], optimizers[3] = airplaneNetwork(.13, [3, 32, 32])        #cat
-models[4], optimizers[4] = airplaneNetwork(.13, [3, 32, 32])        #deer
-models[5], optimizers[5] = airplaneNetwork(.13, [3, 32, 32])        #dog
-models[6], optimizers[6] = airplaneNetwork(.13, [3, 32, 32])        #frog
-models[7], optimizers[7] = airplaneNetwork(.13, [3, 32, 32])        #horse
+models[2], optimizers[2] = airplaneNetwork()        #bird
+models[3], optimizers[3] = airplaneNetwork()        #cat
+models[4], optimizers[4] = airplaneNetwork()        #deer
+models[5], optimizers[5] = airplaneNetwork()        #dog
+models[6], optimizers[6] = airplaneNetwork()        #frog
+models[7], optimizers[7] = airplaneNetwork()        #horse
 models[8], optimizers[8] = shipNetwork()        #ship
-models[9], optimizers[9] = airplaneNetwork(.13, [3, 32, 32])        #truck
+models[9], optimizers[9] = truckNetwork()        #truck
 m, o = allLabelsNetwork(.01, [3, 32, 32])
 
-for i in range(NUM_LABELS):
+for i in [9]:
     print('Training Model #' + str(i+1))
     llt, llv, lltst[i], lbltst = relabelDataPyTorch((loader_train, loader_val, loader_test), i, device)
     train_model(models[i], optimizers[i], device, llt, llv, epochs=NUM_EPOCHS)
 
-for i in range(NUM_LABELS):
+for i in [9]:
     print('Checking Accuracy for Model #' + str(i+1))
     out[i],all_scores[i] = check_accuracy(lltst[i], models[i], device, False)
 
@@ -75,3 +75,8 @@ check_accuracy(loader_test, m, device, False)
 end = time()
 print()
 print('Runtime: %d Minutes and %f Seconds' % (((end-start)//60), ((end-start)%60)))
+
+# Makes noise to indicate completion
+print('\a')
+print('\a')
+print('\a')
--- pytorchNetworks/airplaneNetwork.py
+++ pytorchNetworks/airplaneNetwork.py
@@ -6,33 +6,37 @@ def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
         nn.init.xavier_uniform_(m.weight)
 
-def airplaneNetwork(learning_rate, channels):
+def airplaneNetwork():
     model = nn.Sequential(
-        nn.Conv2d(3, 32, (5, 5), padding=2),
+        nn.BatchNorm2d(3),
         nn.ReLU(),
-        nn.Conv2d(32, 32, (3, 3), padding=1),
+        nn.Conv2d(3, 64, (5, 5), padding=2),
+        nn.BatchNorm2d(64),
         nn.ReLU(),
-        nn.MaxPool2d((2,2), stride=2),
-        nn.Conv2d(32, 16, (7, 7), padding=3),
+        nn.Conv2d(64, 64, (3, 3), padding=1),
+        nn.BatchNorm2d(64),
         nn.ReLU(),
-        nn.Conv2d(16, 24, (5, 5), padding=2),
+        nn.Conv2d(64, 32, (3, 3), padding=1),
+        nn.BatchNorm2d(32),
         nn.ReLU(),
-        nn.MaxPool2d((2,2), stride=2),
-        nn.Conv2d(24, 64, (3, 3), padding=1),
+        nn.Conv2d(32, 24, (5, 5), padding=2),
+        nn.BatchNorm2d(24),
         nn.ReLU(),
-        nn.Conv2d(64, 32, (7, 7), padding=3),
+        nn.Conv2d(24, 64, (7, 7), padding=3),
+        nn.BatchNorm2d(64),
         nn.ReLU(),
-        nn.MaxPool2d((2,2), stride=2),
-        nn.Conv2d(32, 64, (9, 9), padding=4),
+        nn.Conv2d(64, 32, (3, 3), padding=1),
+        nn.BatchNorm2d(32),
         nn.ReLU(),
-        nn.Conv2d(64, 128, (7, 7), padding=3),
+        nn.Conv2d(32, 24, (5, 5), padding=2),
+        nn.BatchNorm2d(24),
         nn.ReLU(),
-        nn.MaxPool2d((4,4), stride=2),
+        nn.Conv2d(24, 16, (5, 5), padding=2),
         Flatten(),
-        nn.Linear(128, 2),
+        nn.Linear(16384, 2),
     )
 
     model.apply(init_weights)
-    optimizer = optim.RMSprop(model.parameters(), lr=.0005)
+    optimizer = optim.Adam(model.parameters(), lr=.0009)
 
     return model, optimizer
