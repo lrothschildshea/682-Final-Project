f235ec82885aceea097b4303d1cc747ea1cb55b1
Liam Rothschild-Shea <lrothschildshea@gmail.com>
2018-11-30 23:52:03 -0500

    it runs but not well

--- pytorchNetworks/catNetwork.py
+++ pytorchNetworks/catNetwork.py
@@ -2,7 +2,11 @@ import torch.nn as nn
 import torch.optim as optim
 from .utils import *
 
-def catNetwork(learning_rate, channels):
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        nn.init.xavier_uniform_(m.weight)
+
+def catNetwork():
     model = nn.Sequential(
         nn.Conv2d(3, 32, 3, 1, 1),
         nn.BatchNorm2d(32),
