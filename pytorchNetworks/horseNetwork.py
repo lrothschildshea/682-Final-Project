51da12de314c164b00c49725d76c88510c4c21f4
Thomas Aurigemma <taaurigemma@gmail.com>
2018-12-01 18:39:28 -0500

    94% Dog, 95+% Frog, 95+% Horse

--- pytorchNetworks/horseNetwork.py
+++ pytorchNetworks/horseNetwork.py
@@ -2,13 +2,40 @@ import torch.nn as nn
 import torch.optim as optim
 from .utils import *
 
-def horseNetwork(learning_rate, channels):
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d:
+        nn.init.xavier_uniform_(m.weight)
+
+def horseNetwork():
     model = nn.Sequential(
-        nn.Conv2d(channels[0], channels[1], (5, 5), padding=2),
-        nn.ReLU(),
+        nn.Conv2d(3, 32, 3, 1, 1),
+        nn.BatchNorm2d(32),
+        nn.Dropout(0.2),
+        nn.LeakyReLU(0.01),
+        nn.ConvTranspose2d(32, 24, 5, 1, 2),
+        nn.BatchNorm2d(24),
+        nn.Dropout(0.2),
+        nn.LeakyReLU(0.01),
+        nn.MaxPool2d(2,2),
+        nn.Conv2d(24, 64, 5, 1, 2),
+        nn.BatchNorm2d(64),
+        nn.Dropout(0.2),
+        nn.LeakyReLU(0.01),
+        nn.ConvTranspose2d(64, 128, 7, 1, 3),
+        nn.BatchNorm2d(128),
+        nn.Dropout(0.2),
+        nn.LeakyReLU(0.01),
+        nn.MaxPool2d(2,2),
         Flatten(),
-        nn.Linear(channels[2]*32*32, 2),
+        nn.Linear(8192, 64),
+        nn.BatchNorm1d(64),
+        nn.ReLU(),
+        nn.Linear(64,64),
+        nn.BatchNorm1d(64),
+        nn.ReLU(),
+        nn.Linear(64,64)
     )
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    model.apply(init_weights)
+    optimizer = optim.Adam(model.parameters(), lr=0.005, betas = (0.85, 0.99))
 
     return model, optimizer
--- pytorchNetworks/utils.py
+++ pytorchNetworks/utils.py
@@ -8,3 +8,17 @@ def flatten(x):
 class Flatten(nn.Module):
     def forward(self, x):
         return flatten(x)
+
+class Unflatten(nn.Module):
+    """
+    An Unflatten module receives an input of shape (N, C*H*W) and reshapes it
+    to produce an output of shape (N, C, H, W).
+    """
+    def __init__(self, N=-1, C=128, H=7, W=7):
+        super(Unflatten, self).__init__()
+        self.N = N
+        self.C = C
+        self.H = H
+        self.W = W
+    def forward(self, x):
+        return x.view(self.N, self.C, self.H, self.W)
