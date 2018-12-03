f5b1af2f84d6b7173f31c953749ca479996ff302
Liam Rothschild-Shea <lrothschildshea@gmail.com>
2018-12-03 15:48:42 -0500

    about 80% for all-labels network

--- pytorchNetworks/allLabelsNetwork.py
+++ pytorchNetworks/allLabelsNetwork.py
@@ -4,23 +4,54 @@ from .utils import *
 
 def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
-        nn.init.xavier_uniform_(m.weight)
+        nn.init.kaiming_normal_(m.weight)
 
 def allLabelsNetwork():
     model = nn.Sequential(
-        nn.Conv2d(3, 32, (5, 5), padding=2),
+        nn.Conv2d(3, 24, (5, 5), padding=2),
         nn.ReLU(),
-        nn.Conv2d(32, 16, (3, 3), padding=1),
+        nn.Dropout(p=.1),
+        nn.Conv2d(24, 32, (3, 3), padding=1),
         nn.ReLU(),
-        nn.MaxPool2d((2, 2), stride=2),
-        nn.Dropout(p=.05),
-        nn.Conv2d(16, 12, (5, 5), padding=2),
+        nn.BatchNorm2d(32),
+        nn.Conv2d(32, 64, (7, 7), padding=3),
         nn.ReLU(),
+        nn.Dropout(p=.1),
+        nn.Conv2d(64, 32, (3, 3), padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.MaxPool2d((2, 2)),
+        nn.Conv2d(32, 24, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.Dropout(p=.1),
+        nn.Conv2d(24, 32, (5, 5), padding=2),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.Conv2d(32, 24, (5, 5), padding=2),
+        nn.ReLU(),
+        nn.Dropout(p=.1),
+        nn.Conv2d(24, 32, (3, 3), padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.MaxPool2d((2, 2)),
+        nn.Conv2d(32, 64, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.Dropout(p=.1),
+        nn.Conv2d(64, 32, (3, 3), padding=1),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.Conv2d(32, 24, (7, 7), padding=3),
+        nn.ReLU(),
+        nn.Dropout(p=.1),
+        nn.Conv2d(24, 32, (5, 5), padding=2),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.MaxPool2d((2, 2)),
         Flatten(),
-        nn.Linear(3072, 10),
+        nn.Linear(512, 10),
     )
 
     model.apply(init_weights)
-    optimizer = optim.RMSprop(model.parameters(), lr=.00011)
+    optimizer = optim.Adam(model.parameters(), lr=.0009)
 
     return model, optimizer
